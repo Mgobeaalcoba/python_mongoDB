98c2e2a9cf740f77e9d699151e61b32f51909e59
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-06-10 12:28:08 -0300

    :art: Best main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     db = conectar_db()
     
     # Crear un nuevo usuario
-    nuevo_usuario = Usuario("Juan Pérez", "juan.perez@example.com", 30)
+    nuevo_usuario = Usuario("Mariano Gonzalez", "mariano.gonzalez@example.com", 40)
     
     # Guardar el usuario en la base de datos
     guardar_usuario(db, nuevo_usuario)
@@ -19,17 +19,25 @@ def conectar_db(context="local"):
         load_dotenv()
         username = os.getenv("MONGO_USERNAME")
         password = os.getenv("MONGO_PASSWORD")
+        database = os.getenv("MONGO_DATABASE")
     else:
         username = os.getenv("MONGO_USERNAME")
         password = os.getenv("MONGO_PASSWORD")
+        database = os.getenv("MONGO_DATABASE")
         
     cliente = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.gggakxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    db = cliente["ProjectTest"] # Nombre de la base de datos
+    db = cliente[f"{database}"] # Nombre de la base de datos
     return db
 
 
-def guardar_usuario(db, usuario):
-    coleccion = db["usuarios"] # Nombre de la colección. Si no existe, se crea automáticamente
+def guardar_usuario(db, usuario, context="local"):
+    if context == "local":
+        load_dotenv()
+        coleccion = os.getenv("MONGO_COLLECTION")
+    else:
+        coleccion = os.getenv("MONGO_COLLECTION")
+    
+    coleccion = db[f"{coleccion}"] # Nombre de la colección. Si no existe, se crea automáticamente
     coleccion.insert_one(usuario.to_dict())
     print("Usuario guardado con éxito")
 
